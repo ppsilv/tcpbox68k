632d8c420f9f33d6e31cd91828fd5b3ea9223ad3
pdsilva <ppsilv@gmail.com>
2025-07-20 22:54:25 -0300

    update memoria rom executando codigo

--- software/test_led.asm
+++ software/test_led.asm
@@ -1,58 +1,33 @@
-; Programa de teste para placa MC68000
-; Sintaxe com 0x para valores hexadecimais
+        ORG     $00000000
+        DC.L    $00880000       ; SP (ignorado sem RAM)
+        DC.L    START           ; PC inicial
 
-        bra     MAIN            ; Pula para o código principal
+START:
+        ; --- Não use pilha! ---
+        ; --- Configuração inicial ---
+        MOVE.B  #$FF,D0        ; Todos LEDs acesos inicialmente
+        MOVE.B  D0,$2000        ; Escreve nos LEDs
 
-; Vetores de inicialização
-VECTOR_INIT:
-        move.l  #0x00880000,a7  ; SP inicial
-        jmp     MAIN            ; PC inicial
-        bra     ERROR_HANDLER   ; Vetor de bus error
-        bra     ERROR_HANDLER   ; Vetor de address error
+MAIN_LOOP:
+        ; --- Padrão 1: Todos apagados ---
+        MOVE.B  #$00,D0
+        MOVE.B  D0,$2000        ; Byte LOW apenas!
 
-MAIN:
-        move.l  #0x00880000,a7  ; Inicializa stack pointer
+        ; Delay preciso (~100ms)
+        MOVE.L  #1600000,D1     ; Ajustado para 16MHz
+DELAY_OFF:
+        SUBQ.L  #1,D1
+        BNE     DELAY_OFF
 
-        ; Teste simples de RAM
-        move.l  #0xA5A5A5A5,d0
-        move.l  d0,0x00080000   ; Endereço da RAM (0x80000)
-        move.l  0x00080000,d1   ; Lê de volta
-        cmp.l   d0,d1
-        bne     ERROR_HANDLER   ; Se diferente, vai para erro
+        ; --- Padrão 2: Todos acesos ---
+        MOVE.B  #$FF,D0
+        MOVE.B  D0,$2000
 
-        ; Loop principal - animação nos LEDs
-LED_LOOP:
-        move.w  #0x0001,d1      ; Padrão inicial
-        move.w  #16,d2          ; Número de LEDs
+        ; Delay igual
+        MOVE.L  #1600000,D1
+DELAY_ON:
+        SUBQ.L  #1,D1
+        BNE     DELAY_ON
 
-LED_ANIMATE:
-        move.w  d1,0x00002000  ; Endereço dos LEDs (0x2000)
-        rol.w   #1,d1           ; Rotaciona o padrão
-
-        ; Delay loop (ajuste conforme clock)
-        move.l  #160000,d3
-DELAY:
-        subq.l  #1,d3
-        bne     DELAY
-
-        dbra    d2,LED_ANIMATE
-        bra     LED_LOOP
-
-; Manipulador de erros (pisca LEDs)
-ERROR_HANDLER:
-        move.w  #0xAAAA,d4
-ERROR_LOOP:
-        move.w  d4,0x00002000   ; Endereço dos LEDs (0x2000)
-        eori.w  #0xFFFF,d4
-        move.l  #3200000,d5
-ERROR_DELAY:
-        subq.l  #1,d5
-        bne     ERROR_DELAY
-        bra     ERROR_LOOP
-
-; Preenchimento com NOPs (exemplo)
-FILLER:
-        nop
-        nop
-        nop
-        ; Continue adicionando NOPs até completar 8KB
+        BRA     MAIN_LOOP
+        END
